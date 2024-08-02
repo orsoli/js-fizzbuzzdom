7308d97771bf1ff50c9321c36af9d536d0995f83
orsoli <fljcoli@gmail.com>
2024-08-02 15:41:13 +0200

    Added preparation phase

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("js ok");
+/*
+    - Retrieve the elements we are interested in from the DOM.
+    - Create the variabels that already know
+    - Create FOR loop to prepare the numbers from 1 to 100.
+    - Create the conditions to print in console fizz and buzz or fizzbuzz
+    - Create the elements to fill the container in dom to print the results
+    - Apply different styles to the elements added to the DOM.
+*/
+
+//--- Preparation phase
+// Retrieve the interested element from DOM
+const container = document.getElementById("container");
+// Create the variables
+const fizz = "Fizz";
+const buzz = "Buzz";
