1922f3fc3fddef1b7ae2cf406d4bc24a421d4970
orsoli <fljcoli@gmail.com>
2024-08-02 17:10:33 +0200

    added print in page process

--- js/index.js
+++ js/index.js
@@ -9,22 +9,38 @@
 
 //--- Preparation phase
 // Retrieve the interested element from DOM
-const container = document.getElementById("container");
+const containerElement = document.getElementById("container");
 // Create the variables
 const fizz = "Fizz";
 const buzz = "Buzz";
 
 //---Processing phase
+//Create elemet div inside container
+const divRow = document.createElement("div");
+divRow.classList.add("row", "row-cols-6", "d-flex", "flex-wrap");
+containerElement.appendChild(divRow);
 // Create forr loop to prepare the numbers
 for (i = 1; i <= 100; i++) {
+  //Create i times the div class='col' inside div row
+  const divCol = document.createElement("div");
+  divCol.classList.add("col", "border", "rounded");
+  divRow.appendChild(divCol);
   //Condition for multiples of 3, 5 or both, print Fizz, Buzz or fizzbuzz instead of the number.
   if (i % 15 === 0) {
     console.log(fizz + buzz);
+    // Print in page
+    divCol.innerHTML = `${fizz}${buzz}`;
   } else if (i % 5 === 0) {
     console.log(buzz);
+    // Print in page
+    divCol.innerHTML = `${buzz}`;
   } else if (i % 3 === 0) {
     console.log(fizz);
+    // Print in page
+    divCol.innerHTML = `${fizz}`;
   } else {
     console.log(i);
+    // Print in page
+    divCol.innerHTML = `${i}`;
   }
 }
