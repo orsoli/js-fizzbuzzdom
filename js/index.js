6f5ab35b32b7c260f60f6ea7e9568d5fb228b64b
orsoli <fljcoli@gmail.com>
2024-08-02 17:44:58 +0200

    added title

--- js/index.js
+++ js/index.js
@@ -13,49 +13,63 @@ const containerElement = document.getElementById("container");
 // Create the variables
 const fizz = "Fizz";
 const buzz = "Buzz";
+//Document title
+const title = document.title;
 
 //---Processing phase
-//Create elemet div inside container
-const divRow = document.createElement("div");
-divRow.classList.add(
+//Create elemets
+const titleElement = document.createElement("h1");
+const divRowElement = document.createElement("div");
+//Added inside container
+containerElement.appendChild(titleElement);
+containerElement.appendChild(divRowElement);
+divRowElement.classList.add(
   "row",
   "row-cols-6",
-  "mx-auto",
+  "justify-content-center",
   "d-flex",
   "flex-wrap",
   "gap-3"
 );
-containerElement.appendChild(divRow);
-// Create forr loop to prepare the numbers
+// Print the title
+titleElement.innerText = title;
+
+// Create for loop to prepare the numbers
 for (i = 1; i <= 100; i++) {
-  //Create i times the div class='col' inside div row
-  const divCol = document.createElement("div");
-  divCol.classList.add("col", "text-center", "border", "rounded", "py-3");
-  divRow.appendChild(divCol);
+  //Create 'i' times the div class='col' inside div row
+  const divColElement = document.createElement("div");
+  divColElement.classList.add(
+    "col",
+    "text-center",
+    "border",
+    "rounded",
+    "py-3"
+  );
+  divRowElement.appendChild(divColElement);
   //Condition for multiples of 3, 5 or both, print Fizz, Buzz or fizzbuzz instead of the number.
   if (i % 15 === 0) {
     console.log(fizz + buzz);
     // Print in page
-    divCol.innerHTML = `${fizz}${buzz}`;
+    divColElement.innerHTML = `${fizz}${buzz}`;
     //Add class for bg color
-    divCol.classList.add("fizzbuzz-bg");
+    divColElement.classList.add("fizzbuzz-bg");
   } else if (i % 5 === 0) {
     console.log(buzz);
     // Print in page
-    divCol.innerHTML = `${buzz}`;
+    divColElement.innerHTML = `${buzz}`;
     //Add class for bg color
-    divCol.classList.add("buzz-bg");
+    divColElement.classList.add("buzz-bg");
   } else if (i % 3 === 0) {
     console.log(fizz);
     // Print in page
-    divCol.innerHTML = `${fizz}`;
+    divColElement.innerHTML = `${fizz}`;
     //Add class for bg color
-    divCol.classList.add("fizz-bg");
+    divColElement.classList.add("fizz-bg");
   } else {
     console.log(i);
     // Print in page
-    divCol.innerHTML = `${i}`;
+    divColElement.innerHTML = `${i}`;
     //Add class for bg color
-    divCol.classList.add("nr-bg");
+    divColElement.classList.add("nr-bg");
   }
 }
